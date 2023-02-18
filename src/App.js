24aa7930780e93092137afd81f83e03a6577fdb7
Kate Bennert <kate.bennert@gmail.com>
2023-02-18 15:43:03 -0500

    Fetch test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+
 
 function App() {
+
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:9292/freelancers")
+      .then((r) => r.json())
+      .then((jobs) => setJobs(jobs));
+  }, []);
+
+  console.log(jobs)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World!</h1>
+      {jobs.map(job => (
+        <div>
+          <p>{job.name}</p>
+          <img src={job.image_url} alt="" width="500" height="600"/>
+        </div>
+        ))}
     </div>
   );
 }
