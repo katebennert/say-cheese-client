77fb999c08a6e3390adc48082f485428378f4093
Kate Bennert <kate.bennert@gmail.com>
2023-02-24 13:04:28 -0500

    Add React Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,30 @@
 import '../App.css';
 import React, { useEffect } from "react";
+import { Route, Switch } from "react-router-dom";
 import './JobList';
 import './FreelancerList';
 import './CreateJob';
+import './NavBar';
 
 function App() {
 
   return (
     <div>
-      <h1>Hello World! Yo</h1>
+      <NavBar />
+      <Switch>
+        <Route path="/jobs">
+          <Jobs />
+        </Route>
+        <Route path="/freelancers">
+          <Freelancers />
+        </Route>
+        <Route path="/create-job">
+          <CreateJob />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
