86e5a54255cb1a51bdc9b44dc78eaff9b5b4a0c1
Kate Bennert <kate.bennert@gmail.com>
2023-03-10 11:53:56 -0500

    Create new job form

--- src/components/CreateJob.js
+++ src/components/CreateJob.js
@@ -2,9 +2,50 @@ import React from "react";
 
 function CreateJob() {
   return (
-    <div>
-      <h1>Create Job</h1>
-    </div>
+    <form className="create-job">
+        <label> Name:
+            <input type="text" name="name" value="" autoComplete="off"
+                // value={messageBody}
+                // onChange={(e) => setMessageBody(e.target.value)}
+            />
+        </label>
+      <input type="text" name="company" value="" autoComplete="off"
+        // value={messageBody}
+        // onChange={(e) => setMessageBody(e.target.value)}
+      />
+      <input type="text" name="company-logo" value="" autoComplete="off"
+        // value={messageBody}
+        // onChange={(e) => setMessageBody(e.target.value)}
+      />
+      <input type="text" name="start-date" value="" autoComplete="off"
+        // value={messageBody}
+        // onChange={(e) => setMessageBody(e.target.value)}
+      />
+      <input type="text" name="end-date" value="" autoComplete="off"
+        // value={messageBody}
+        // onChange={(e) => setMessageBody(e.target.value)}
+      />
+      <input type="text" name="description" value="" autoComplete="off"
+        // value={messageBody}
+        // onChange={(e) => setMessageBody(e.target.value)}
+      />
+      <input type="text" name="freelancers-needed" value="" autoComplete="off"
+        // value={messageBody}
+        // onChange={(e) => setMessageBody(e.target.value)}
+      />
+      <input type="submit" value="Save" />
+      {/**
+       * name
+       * company
+       * company logo
+       * start date
+       * end date
+       * description
+       * freelancers needed
+       * is full = false
+       * isopen = true or based on start and end
+       */}
+    </form>
   )
 }
 
