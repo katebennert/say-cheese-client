b051b5db0a1d44dec66dd9fc2cac97bd0e595333
Kate Bennert <kate.bennert@gmail.com>
2023-03-12 15:37:22 -0400

    Fix form values

--- src/components/CreateJob.js
+++ src/components/CreateJob.js
@@ -1,37 +1,114 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreateJob() {
+
+    const [newJob, setNewJob] = useState({
+        name: "",
+        company: "",
+        companyLogo: "",
+        startDate: null,
+        endDate: null,
+        description: "",
+        freelancersNeeded: null
+    });
+
+    function handleChange(e) {
+        const name = e.target.name;
+        let value = e.target.value;
+
+        // if dropdown another thing
+
+        setNewJob({
+            ...newJob,
+            [name]: value
+        });
+
+        console.log(newJob)
+    }
 
   return (
     <div className="new-job-form">
         <h2>✨ Create New Job ✨</h2>
         <form>
             <div className="inputbox">
-                <input type="text" required="required"/>
+                <input 
+                    type="text" 
+                    name="name" 
+                    autoComplete="off"
+                    value={newJob.name}
+                    onChange={handleChange} 
+                    required="required"
+                />
                 <span>Job Name</span>
             </div>
             <div className="inputbox">
-                <input type="text" required="required"/>
+                <input 
+                    type="text" 
+                    name="company" 
+                    autoComplete="off"
+                    value={newJob.company}
+                    onChange={handleChange} 
+                    required="required"
+                />
                 <span>Company</span>
             </div>
             <div className="inputbox">
-                <input type="text" required="required"/>
+                <input 
+                    type="text" 
+                    name="companyLogo" 
+                    autoComplete="off"
+                    value={newJob.companyLogo}
+                    onChange={handleChange} 
+                    required="required"
+                />
                 <span>Company Logo URL</span>
             </div>
             <div className="inputbox">
-                <input type="text" required="required"/>
+                <input 
+                    type="text" 
+                    name="description" 
+                    autoComplete="off"
+                    value={newJob.description}
+                    onChange={handleChange} 
+                    required="required"
+                />
+                <span>Job Description</span>
+            </div>
+            <div className="inputbox">
+                <input 
+                    type="text" 
+                    name="startDate" 
+                    autoComplete="off"
+                    value={newJob.startDate}
+                    onChange={handleChange} 
+                    required="required"
+                />
                 <span>Job Start Date</span>
             </div>
             <div className="inputbox">
-                <input type="text" required="required"/>
+                <input 
+                    type="text" 
+                    name="endDate" 
+                    autoComplete="off"
+                    value={newJob.endDate}
+                    onChange={handleChange} 
+                    required="required"
+                />
                 <span>Job End Date</span>
             </div>
             <div className="inputbox">
-                <input type="text" required="required"/>
+                <input 
+                    type="text" 
+                    name="freelancersNeeded" 
+                    autoComplete="off"
+                    value={newJob.freelancersNeeded}
+                    onChange={handleChange} 
+                    required="required"
+                />
                 <span>Freelancers Needed</span>
             </div>
             <div className="inputbox">
-                <input type="button" value="submit"/>
+                <button type="submit">Create Job</button>
             </div>
         </form>
     </div>
@@ -39,49 +116,3 @@ function CreateJob() {
 }
 
 export default CreateJob;
-
-    {/* <form className="create-job">
-        <label> Job Name:
-            <input type="text" name="name" value="" autoComplete="off"
-                value={messageBody}
-                onChange={(e) => setMessageBody(e.target.value)}
-            />
-        </label>
-        <label> Company:
-            <input type="text" name="company" value="" autoComplete="off"
-                value={messageBody}
-                onChange={(e) => setMessageBody(e.target.value)}
-            />
-        </label>
-        <label> Logo URL:
-            <input type="text" name="company-logo" value="" autoComplete="off"
-                value={messageBody}
-                onChange={(e) => setMessageBody(e.target.value)}
-            />
-        </label>
-        <label> Job Start Date:
-            <input type="text" name="start-date" value="" autoComplete="off"
-                value={messageBody}
-                onChange={(e) => setMessageBody(e.target.value)}
-            />
-         </label>
-         <label> Job End Date:
-            <input type="text" name="end-date" value="" autoComplete="off"
-                value={messageBody}
-                onChange={(e) => setMessageBody(e.target.value)}
-            />
-         </label>
-         <label> Job End Date:
-            <input type="text" name="description" value="" autoComplete="off"
-                value={messageBody}
-                onChange={(e) => setMessageBody(e.target.value)}
-            />
-         </label>
-         <label> Freelancers Needed:
-            <input type="text" name="freelancers-needed" value="" autoComplete="off"
-                value={messageBody}
-                onChange={(e) => setMessageBody(e.target.value)}
-            />
-         </label>
-      <input type="submit" value="Save" />
-    </form>  */}
